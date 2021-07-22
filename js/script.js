5766c8fd6f35c9af663af78e9841c571f9d1d7a0
Mostakimul Karim <58774850+Mostakimul@users.noreply.github.com>
2021-07-23 00:33:10 +0200

    general answer check working

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const correctAnswer = ['A', 'B', 'C', 'C'];
+const form = document.querySelector('.quiz-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let score = 0;
+  // get users answer
+  const userAnswer = [
+    form.q1.value,
+    form.q2.value,
+    form.q3.value,
+    form.q4.value,
+  ];
+
+  // answer check
+  userAnswer.forEach((answer, index) => {
+    if (answer === correctAnswer[index]) {
+      score += 25;
+    }
+  });
+
+  console.log(score);
+});
